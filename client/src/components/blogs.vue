f5caaa571ea7bb43fd43be120b614e019e01607a
unknown <ehabzaferabed@gmail.com>
2024-08-08 13:31:14 +0300

    edit blogs

--- client/src/components/blogs.vue
+++ client/src/components/blogs.vue
@@ -1,10 +1,34 @@
 <template>
   <div>
-    <p class="text-4xl">HI</p>
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
   </div>
 </template>
+
 <script>
+import axios from "axios";
+
 export default {
   name: "AllBlogs",
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get("http://localhost:8000/getblog", {
+          blogs: this.blogs,
+        });
+        console.log(response.data);
+      } catch (err) {
+        console.error("error fetching data", err);
+      }
+    },
+  },
 };
 </script>
