c4fc2e97062a9e13440ba3d466d56a6d747a75b3
unknown <ehabzaferabed@gmail.com>
2024-08-10 15:45:31 +0300

    edit blog details

--- client/src/components/details.vue
+++ client/src/components/details.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <div class="flex flex-wrap justify-center">
+    <div v-if="loading" class="text-center text-4xl">Loading ...</div>
+    <div v-else class="flex flex-wrap justify-center">
       <img :src="blog.image" alt="img" class="size-80 w-[528px] h-[460px]" />
-      <div>
+      <div class="flex flex-wrap flex-col gap-4 ml-8">
         <div class="font-bold text-2xl text-center mb-2">
           Blog Title :
           {{ blog.title?.charAt(0).toUpperCase() + blog.title?.slice(1) }}
@@ -29,6 +30,19 @@
             {{ blog.description }}
           </span>
         </div>
+        <div class="flex flex-wrap gap-4">
+          <button
+            class="text-white bg-blue-600 hover:bg-blue-500 w-16 mt-4 rounded-lg"
+          >
+            Edit
+          </button>
+          <button
+            @click="deleteBlog"
+            class="text-white bg-red-600 hover:bg-red-500 w-16 mt-4 rounded-lg"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -36,26 +50,46 @@
 
 <script>
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export default {
   name: "BlogDetails",
   data() {
     return {
       blog: [],
-      id: "",
+      id: this.$route.params.id,
+      loading: true,
+      token: Cookies.get("token"),
     };
   },
   async created() {
-    this.id = this.$route.params.id;
     await this.fetchData();
   },
   methods: {
+    async deleteBlog() {
+      try {
+        await axios.put(
+          `http://localhost:8000/deleteblog/${this.id}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${this.token}`,
+            },
+          }
+        );
+        window.alert("Deleted Successfully");
+        this.$router.push("/");
+      } catch (err) {
+        console.error("error deleting", err);
+      }
+    },
     async fetchData() {
       try {
         const response = await axios.get(
           `http://localhost:8000/getblogid/${this.id}`
         );
         this.blog = response.data;
+        this.loading = false;
       } catch (err) {
         console.error("error fetching data", err);
       }
