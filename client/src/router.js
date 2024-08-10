705b01974444af07c76950e659513af751a105b6
unknown <ehabzaferabed@gmail.com>
2024-08-10 12:43:30 +0300

    add blog details

--- client/src/router.js
+++ client/src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import LogIn from "./components/login.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 import Signup from "./components/signup.vue";
+import BlogDetails from "./components/details.vue";
 
 const routes = [
   { path: "/", component: HelloWorld },
   { path: "/login", component: LogIn },
   { path: "/signup", component: Signup },
+  { path: `/:id`, component: BlogDetails },
 ];
 
 const router = createRouter({
