89ac403d80240f7a77b2eda885ea54b13f9eeb58
unknown <ehabzaferabed@gmail.com>
2024-08-24 14:39:20 +0300

    add header UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,16 @@
 <template>
+  <nav-bar />
   <!-- <p class="text-center text-blue-600 font-black text-4xl">how are you</p> -->
   <router-view></router-view>
 </template>
 
 <script>
+import NavBar from "./components/header.vue";
 export default {
   name: "App",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
---
+++ client/src/components/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="h-16 bg-slate-500">ss</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
